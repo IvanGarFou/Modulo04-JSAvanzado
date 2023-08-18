7e9d9a3ebbebcd898c4599eec04a18b02e4148e1
DoloresPGz <dolores.pgz@gmail.com>
2023-08-17 21:28:57 -0600

    comparando variables con json

---
+++ Clase 04/json.js
@@ -0,0 +1,23 @@
+var alumnos = [
+    {
+        nombre: 'Ana',
+        apellido: 'Lopez',
+        edad: 20,
+        materias: ['español', 'ingles', 'matematicas']
+    },
+    {
+        nombre: 'Pedro',
+        apellido: 'Gutierrez',
+        edad: 20,
+        materias: ['español', 'ingles', 'matematicas']
+    },
+    {
+        nombre: 'Luis',
+        apellido: 'Martinez',
+        edad: 21,
+        materias: ['español', 'ingles', 'matematicas']
+    },
+]
+
+
+//convertir el arreglo en formato json con https://jsonformatter.curiousconcept.com/
